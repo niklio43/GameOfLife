e2a3db013e9f3c63ff085810e60016f349fdfecd
Niklas Jörgensen <niklio15csgo@hotmail.com>
2021-10-27 21:15:41 +0200

    uploaded premature version of gol

---
+++ Assets/GameOfLife.cs
@@ -0,0 +1,142 @@
+using UnityEngine;
+
+public class GameOfLife : ProcessingLite.GP21
+{
+	GameCell[,] cells;
+	float cellSize = 0.25f;
+	int numberOfColums, numberOfRows;
+	int spawnChancePercentage = 15;
+
+    void Start()
+	{
+		QualitySettings.vSyncCount = 0;
+		Application.targetFrameRate = 4;
+
+		numberOfColums = (int)Mathf.Floor(Width / cellSize);
+		numberOfRows = (int)Mathf.Floor(Height / cellSize);
+
+		cells = new GameCell[numberOfColums, numberOfRows];
+
+		for (int y = 0; y < numberOfRows; y++)
+		{
+			for (int x = 0; x < numberOfColums; x++)
+			{
+				cells[x, y] = new GameCell(x * cellSize, y * cellSize, cellSize);
+
+				if (Random.Range(0, 100) < spawnChancePercentage)
+				{
+					cells[x, y].alive = true;
+				}
+			}
+		}
+	}
+
+    void Update()
+    {
+        Background(0);
+
+        for (int y = 1; y < numberOfRows - 1; ++y)
+        {
+            for (int x = 1; x < numberOfColums - 1; ++x)
+            {
+                cells[x, y].last = Adjacent(x, y);
+            }
+        }
+
+        for (int y = 0; y < numberOfRows; ++y)
+        {
+            for (int x = 0; x < numberOfColums; ++x)
+            {
+                if (cells[x, y].last < 2 || cells[x, y].last > 3)
+                {
+                    cells[x, y].alive = false;
+                }
+
+                if (cells[x, y].alive == false && cells[x, y].last == 3)
+                {
+                    cells[x, y].alive = true;
+                }
+            }
+        }
+
+        for (int y = 0; y < numberOfRows; ++y)
+        {
+            for (int x = 0; x < numberOfColums; ++x)
+            {
+                cells[x, y].Draw();
+            }
+        }
+    }
+
+    public int Adjacent(int x, int y)
+    {
+        int adjacent = 0;
+
+                if (cells[x + 1, y + 1].alive)
+                {
+                    adjacent++;
+                }
+
+                if (cells[x + 1, y].alive)
+                {
+                    adjacent++;
+                }
+
+                if (cells[x, y + 1].alive)
+                {
+                    adjacent++;
+                }
+
+                if (cells[x - 1, y - 1].alive)
+                {
+                    adjacent++;
+                }
+
+                if (cells[x - 1, y].alive)
+                {
+                    adjacent++;
+                }
+
+                if (cells[x, y - 1].alive)
+                {
+                    adjacent++;
+                }
+
+                if (cells[x + 1, y - 1].alive)
+                {
+                    adjacent++;
+                }
+
+                if (cells[x - 1, y + 1].alive)
+                {
+                    adjacent++;
+                }
+
+        return adjacent;
+    }
+}
+
+public class GameCell : ProcessingLite.GP21
+{
+	float x, y;
+	float size;
+    public int last;
+    public bool alive = false;
+
+	public GameCell(float x, float y, float size)
+	{
+
+		this.x = x + size / 2;
+		this.y = y + size / 2;
+
+		this.size = size / 2;
+	}
+
+	public void Draw()
+	{
+		if (alive)
+		{
+            Circle(x, y, size);
+		}
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # GameOfLife
+https://gyazo.com/4624eb64198032bb6d1d338e123641ed
